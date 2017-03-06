782541ef687258dec29cab427ab6289a99aefedb
dunyuling <taxuewuhen.com.cn@gmail.com>
2017-03-06 18:32:29 +0800

    first init

---
+++ src/main/java/com/aifeng/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.aifeng.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public @ResponseBody
+    String test() {
+        return "hello, world! This com from spring!";
+    }
+
+    @RequestMapping("/hello1")
+    public String test1(Model model) {
+        model.addAttribute("message","lhg");
+        return "message";
+    }
+}
